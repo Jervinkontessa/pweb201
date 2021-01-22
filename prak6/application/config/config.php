4afc3a6ac7af0a81b1af6f9ef6977cce220bf3d5
Jervinkontessa <Jervindescrates@gmail.com>
2021-01-22 11:09:44 +0800

    edit lat 6

--- prak6/application/config/config.php
+++ prak6/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pweb201/srv2/html/ci3/';
+$config['base_url'] = 'http://localhost/pweb201/srv1/html/prak6/';
 
 /*
 |--------------------------------------------------------------------------
